36fd57fe10425e4200385870a80929a5dc6aa301
alanmac17 <alan17wilson@gmail.com>
2022-10-08 21:18:24 +0000

    updated allowed hosts to resolve deployment issue, removed https//

--- RI_PET/settings.py
+++ RI_PET/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://ri-pet.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ri-pet.herokuapp.com', 'localhost']
 
 
 # Application definition
