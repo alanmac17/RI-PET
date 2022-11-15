cf2f64629279c42cadc5cc064dc258879a3e8d5a
alanmac17 <alan17wilson@gmail.com>
2022-11-15 23:06:08 +0000

    add templates and css file

--- RI_PET_BLOG/admin.py
+++ RI_PET_BLOG/admin.py
@@ -1,24 +1,20 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import DeathNotice, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(DeathNotice)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'death_date')
     search_fields = ['title', 'content']
-    list_filter = ('status', 'created_on')
-    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'death_date')
     summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body', 'post', 'created_on', 'approved')
-    list_filter = ('approved', 'created_on')
+    list_display = ('name', 'body', 'deathnotice', 'created_on')
+    list_filter = ('created_on', 'name')
     search_fields = ('name', 'email', 'body')
-    actions = ['approve_comments']
 
-    def approve_comments(self, request, queryset):
-        queryset.update(approved=True)
--- RI_PET_BLOG/views.py
+++ RI_PET_BLOG/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import DeathNotice
 
-# Create your views here.
+class DeathNoticeList (generic.ListView):
+    model = DeathNotice
+    queryset = DeathNotice.objects.filter(status=1).order_by('-death_date')
+    template_name = 'index.html'
+    paginate_by = 6
+
+
+
